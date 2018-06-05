42676d7cdd4ac8e0eebfaf7c533b6982e38b01a1
Aourell <marion.wilhelm.57@gmail.com>
2018-06-05 22:40:21 +0200

    Derniere ligne droite

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -1,18 +1,56 @@
-void	ft_putchar(char c);
+void	ft_putchar (char c);
+
+void	ft_print_nb(int nb)
+{
+	int		U;
+	int		D;
+
+	D = nb / 10;
+	U = nb % 10;
+	if (nb <= 9)
+	{
+		ft_putchar('0');
+		ft_putchar(U + 48);
+	}
+	else
+	{
+		ft_putchar(D + 48);
+		ft_putchar(U + 48);
+	}
+}
+
+void	ft_print_final(int nb1, int nb2)
+{
+	if (nb1 == 98 && nb2 == 99)
+	{
+		ft_print_nb(nb1);
+		ft_putchar(' ');
+		ft_print_nb(nb2);
+	}
+	else
+	{
+	ft_print_nb(nb1);
+	ft_putchar(' ');
+	ft_print_nb(nb2);
+	ft_putchar(',');
+	ft_putchar(' ');
+	}
+}
 
 void	ft_print_comb2(void)
 {
-	int	a;
-	int	b;
-	int	c;
-	int	d;
+	int		nb1;
+	int		nb2;
 
-	a = 0;
-	b = 0;
-	c = 0;
-	d = 0;
-	while (a <= 9 || B <= 9)
+	nb1 = 0;
+	while (nb1 <= 99)
 	{
-
+		nb2 = nb1 + 1;
+		while (nb2 <= 99)
+		{
+			ft_print_final(nb1, nb2);
+			nb2++;
+		}
+		nb1++;
 	}
 }
