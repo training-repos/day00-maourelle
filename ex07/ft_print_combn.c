07ab2baf88fa947defe28a3af0b6acc2e9745e61
Aourell <marion.wilhelm.57@gmail.com>
2018-06-05 23:29:09 +0200

    je pense pas pouvoir le finir

---
+++ ex07/ft_print_combn.c
@@ -0,0 +1,20 @@
+void	ft_putchar(char c);
+
+void	ft_print_combn(int	n)
+{
+	int i;
+
+	i = 0;
+	if (n == 1)
+		while (i <= 9)
+		{
+			ft_putchar(i + 48);
+			i++;
+		}
+	else if (n <= 0 || n >= 10 )
+		ft_putchar('0');
+	else
+		{
+			while(
+		}
+}
