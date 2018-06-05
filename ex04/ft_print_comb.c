1f1b5d83dcdb36a32f2438818c40e4e93ee89027
Aourell <marion.wilhelm.57@gmail.com>
2018-06-05 21:32:19 +0200

    Enfin, il n'y a plus d'erreur

--- ex04/ft_print_comb.c
+++ ex04/ft_print_comb.c
@@ -1,33 +1,44 @@
-void	ft_putchar (char c);
+#include <unistd.h>
 
-void	ft_space(int U, int D, int C)
+void	ft_putchar (char c)
 {
+	write(1, &c, 1);
+}
+
+void	ft_print_CDU(int C, int D, int U)
+{
+	if (C == 7 && D == 8 && U == 9)
+	{
 		ft_putchar(C + 48);
 		ft_putchar(D + 48);
 		ft_putchar(U + 48);
+	}
+	else
+	{
+	ft_putchar(C + 48);
+	ft_putchar(D + 48);
+	ft_putchar(U + 48);
+	ft_putchar(',');
+	ft_putchar(' ');
+	}
 }
 
 void	ft_print_comb(void)
 {
-	int U;
-	int D;
-	int C;
+	int		U;
+	int		D;
+	int		C;
 
 	C = 0;
-	while (C <= 7)
+	while (C <= 9)
 	{
 		D = C + 1;
-		while (D <= 8)
+		while (D <= 9)
 		{
 			U = D + 1;
 			while (U <= 9)
 			{
-				ft_space(C, D, U);
-				while (C != 7 && D != 8 && U != U)
-					{
-						ft_putchar(',');
-						ft_putchar(' ');
-					}
+				ft_print_CDU(C, D, U);
 				U++;
 			}
 			D++;
